3af44a8e197e5012795a208f19d72178343420af
niadulMunna <niadulislammunna@gmail.com>
2025-04-07 23:23:08 +0600

    refactor

--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,10 +11,49 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { z, ZodType } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const schema: ZodType<FormData> = z
+    .object({
+      name: z
+        .string()
+        .min(2, { message: "Name must be at least 2 characters" }),
+      email: z.string().email(),
+      password: z
+        .string()
+        .min(4, { message: "Password must be at least 4 characters" })
+        .max(32),
+      confirmPassword: z.string().min(4).max(32),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+  const submitData: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+    console.log("Submitted");
+  };
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -22,17 +62,29 @@ export function SignupForm({
           <CardDescription>Create an account</CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit(submitData)}>
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
                 <Label htmlFor="Name">Name</Label>
-                <Input id="name" type="text" placeholder="John due" required />
+                <Input
+                  id="name"
+                  type="text"
+                  {...register("name")}
+                  placeholder="John due"
+                  required
+                />
+                {errors.name && (
+                  <p className="text-destructive text-sm">
+                    {errors.name.message}
+                  </p>
+                )}
               </div>
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
                 <Input
                   id="email"
                   type="email"
+                  {...register("email")}
                   placeholder="m@example.com"
                   required
                 />
@@ -41,13 +93,23 @@ export function SignupForm({
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
                 </div>
-                <Input id="password" type="password" required />
+                <Input
+                  id="password"
+                  type="password"
+                  {...register("password")}
+                  required
+                />
               </div>
               <div className="grid gap-3">
                 <div className="flex items-center">
                   <Label htmlFor="confirmPassword">Confirm Password</Label>
                 </div>
-                <Input id="confirmPassword" type="password" required />
+                <Input
+                  id="confirmPassword"
+                  type="password"
+                  {...register("confirmPassword")}
+                  required
+                />
               </div>
               <div className="flex flex-col gap-3">
                 <Button type="submit" className="w-full">
