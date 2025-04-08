6415070acd49d7e1a5f7cf712a5917dcce428048
niadulMunna <niadulislammunna@gmail.com>
2025-04-08 23:47:56 +0600

    login schema

---
+++ src/schema/login-schema.ts
@@ -0,0 +1,9 @@
+import * as z from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(4, { message: "Password must be at least 4 characters" })
+    .max(32),
+});
