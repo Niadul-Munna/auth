6e1163802ac9bfe3212a2bbc1727623519e1157b
niadulMunna <niadulislammunna@gmail.com>
2025-04-09 00:21:45 +0600

    style(home): add css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/auth/login-form";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex h-screen w-full items-center justify-center">
       <LoginForm />
     </div>
   );
