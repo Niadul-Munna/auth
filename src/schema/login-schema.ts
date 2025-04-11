1581bae2b11ea8567c7fb12546a2cb383cdafcc2
niadulMunna <niadulislammunna@gmail.com>
2025-04-11 20:09:18 +0600

    schema(login):  remove email and add userName

--- src/schema/login-schema.ts
+++ src/schema/login-schema.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email(),
+  userName: z.string(),
   password: z
     .string()
     .min(4, { message: "Password must be at least 4 characters" })
